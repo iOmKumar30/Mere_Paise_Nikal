1a94b3dcd75c525f7cbc3d23c4c69d4701ca940b
iOmKumar30 <iomkumar03@outlook.com>
2024-08-21 21:01:14 +0530

    Fixing issues

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://mere-paise-nikal-backend.onrender.com/',
+  optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
