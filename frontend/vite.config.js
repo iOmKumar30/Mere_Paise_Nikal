b8698c189cc259492b8aa617169e6a612fdede54
iOmKumar30 <iomkumar03@outlook.com>
2024-08-21 12:59:23 +0530

    Fixing issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
+      external: ["react-router-dom"],
     },
   },
-})
+  optimizeDeps: {
+    include: ["react-router-dom"],
+  },
+
+  base: "./",
+});
